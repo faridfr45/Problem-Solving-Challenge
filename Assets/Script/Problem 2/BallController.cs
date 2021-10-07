51e5f3da3730b7b9644b8668d50b61f8fbcd3493
FaridRey <89561572+faridfr45@users.noreply.github.com>
2021-10-07 18:39:50 +0700

    Add ball movement

---
+++ Assets/Script/Problem 2/BallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        PushBall();
+    }
+
+    // Bola akan bergerak ke arah yang random dengan kecepatan yang konstan
+    void PushBall(){
+        float x = Random.Range(-100, 100);
+        float y = Random.Range(-100, 100);
+
+        rb.AddForce(new Vector2(x, y).normalized * 50);
+    }
+}
