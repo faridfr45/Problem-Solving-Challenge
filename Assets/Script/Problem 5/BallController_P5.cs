958a3b7ab5c888ca09e04c50b794953cb730fc63
FaridRey <89561572+faridfr45@users.noreply.github.com>
2021-10-07 19:44:00 +0700

    Add ball follow cursor script

--- Assets/Script/Problem 5/BallController_P5.cs
+++ Assets/Script/Problem 5/BallController_P5.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController_P4 : MonoBehaviour
+public class BallController_P5 : MonoBehaviour
 {
     public float speed = 10f;
+
+    [Header("Control Type")]
+    public bool useMouse;
 
     Rigidbody2D ballRb;
     Vector3 movement;
@@ -19,7 +22,12 @@ public class BallController_P4 : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        Move(x, y);
+        // Jika variabel useMouse bernilai false, 
+        // maka kontrol bola akan menggunakan keyboard
+        if(!useMouse)
+            Move(x, y);
+        else
+            FollowMouse();
     }
 
     public void Move(float hor, float ver){
@@ -28,5 +36,11 @@ public class BallController_P4 : MonoBehaviour
         movement = movement.normalized * speed * Time.deltaTime;
 
         ballRb.MovePosition(transform.position + movement);
+    }
+
+    // agar bola mengikuti mouse
+    public void FollowMouse(){
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
     }
 }
