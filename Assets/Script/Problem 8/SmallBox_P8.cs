3e5f925183aaf9ad08bfcfca7b0788b061ba3645
FaridRey <89561572+faridfr45@users.noreply.github.com>
2021-10-08 11:00:07 +0700

    Update problem 8

---
+++ Assets/Script/Problem 8/SmallBox_P8.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallBox_P8 : MonoBehaviour
+{
+    private void OnDisable() {
+        Invoke("SpawnRandom", 3);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player"){
+
+            this.gameObject.SetActive(false);
+            ScoreManager.Instance.AddScore();
+        }
+    }
+
+    private void SpawnRandom(){
+        float randomX, randomY;
+        Vector3 newPos;
+        bool spawned = false;
+        GameObject ball = GameObject.FindGameObjectWithTag("Player");
+
+        // saya menggunakan while untuk spawn box
+        while(!spawned){
+            randomX = Random.Range(-7, 7);
+            randomY = Random.Range(-4, 4);
+
+            newPos = new Vector3(randomX, randomY, 0f);
+            this.transform.position = newPos;
+
+            // jika posisi smallBox terlalu dekat ataupun didalam lingkaran
+            // maka posisi yang baru akan dicari
+            if((transform.position - ball.transform.position).magnitude < 2){
+                continue;
+            }else{
+                spawned = true;
+            }
+        }
+        this.gameObject.SetActive(true);
+    }
+
+}
