cecc8a57e8332829467da639c167073e2bbc6fba
FaridRey <89561572+faridfr45@users.noreply.github.com>
2021-10-11 00:22:56 +0700

    Update Main Menu

---
+++ Assets/Script/Problem 9/LevelSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject levelSelect;
+
+    public void Play(){
+        menu.SetActive(false);
+        levelSelect.SetActive(true);
+    }
+
+    public void Back(){
+        menu.SetActive(true);
+        levelSelect.SetActive(false);
+    }
+}
